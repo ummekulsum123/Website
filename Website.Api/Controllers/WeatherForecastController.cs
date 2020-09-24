8a7797fc9c06e78b03f89821d5f9b22475b67ede
alexsey chuvilkin <leha.chuvilkin.90@mail.ru>
2020-09-24 23:45:37 +0300

    Added dockerfile and doker-compose.

--- Website.Api/Controllers/WeatherForecastController.cs
+++ Website.Api/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Website.API.Controllers
+namespace Website.Api.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
--- Website.Api/WeatherForecast.cs
+++ Website.Api/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Website.API
+namespace Website.Api
 {
 	public class WeatherForecast
 	{
