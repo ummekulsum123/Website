80dd68beddd6e667be326b9abcd590fc55cafa41
lorderor <gosha_ppp@bk.ru>
2020-09-28 22:14:58 +0300

    clean App

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import styled from "styled-components";
 import HomePage from "./containers/HomePage";
 
 function App() {
   return <Route component={HomePage} />;
 }
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #f0f4f7;
-  min-height: 100vh;
-`;
 
 export default App;
